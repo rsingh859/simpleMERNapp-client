e79b875d52ed0d9cbe1435cbe4c59f0c0a3345c0
rsingh859 <61348206+rsingh859@users.noreply.github.com>
2021-01-01 01:58:22 +0530

    Posting to server

---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Post from './Post/Post';
+import useStyles from './styles';
+import { useSelector } from 'react-redux';
+
+const Posts = () => {
+    const classes = useStyles();
+    const posts = useSelector((state) => state.posts);
+
+    console.log(posts);
+
+    return(
+        <React.Fragment>
+            <h1>Posts</h1>
+            <Post />
+            <Post />
+        </React.Fragment>
+    );
+}
+
+export default Posts;
