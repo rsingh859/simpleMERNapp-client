89156ea1e11a830df2f85ecd6fe11112cda81fce
rsingh859 <61348206+rsingh859@users.noreply.github.com>
2021-01-08 00:22:43 +0530

    Final Deployment

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
-const url = 'http://localhost:4000/posts';
+const url = 'https://memories-proj435.herokuapp.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
 export const updatePost = (id, postData) => axios.patch(`${url}/${id}`, postData);
 export const deletePost = (id) => axios.delete(`${url}/${id}`);
+export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './index.css';
 import App from './App';
 import reducers from './reducers';
 
-const store = createStore(reducers, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
     <Provider store={store}>
